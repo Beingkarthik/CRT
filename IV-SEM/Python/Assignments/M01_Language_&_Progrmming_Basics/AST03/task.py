2b8aeccb91436dcc0a0abc2dbd41699b9ec33473
Beingkarthik <2401100cse0155@cse.suh.edu.in>
2026-03-09 11:17:00 +0530

    Message for commit:

--- IV-SEM/Python/Assignments/M01_Language_&_Progrmming_Basics/AST03/task.py
+++ IV-SEM/Python/Assignments/M01_Language_&_Progrmming_Basics/AST03/task.py
@@ -1,10 +1,10 @@
-def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
-    avg = round((n1 + n2 + n3) / 3, 2)
-    status = "Pass" if min(n1, n2, n3) >= 35 else "Fail"
-    return f"Average grade: {avg}, Status: {status}"
+def Student_Grade_System(name:str,n1: int,n2: int,n3: int) -> str:
+   avg = round((n1 + n2 + n3) / 3, 2)
+   status = "Pass" if min(n1, n2, n3) >= 35 else "Fail"
+   return f"Average grade: {avg}, Status: {status}"
 
 
 if __name__ == '__main__':
-    name = input()
-    n1, n2, n3 = list(map(int, input().split()))
-    print(Student_Grade_System(name, n1, n2, n3))
+    name = input("Enter student name: ")
+    n1, n2, n3 = list(map(int, input('Enter three grades separated by spaces: ').split()))
+    print(Student_Grade_System(name,n1,n2,n3))
